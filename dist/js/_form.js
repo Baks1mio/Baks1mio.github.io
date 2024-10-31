0a751ab803cdafb0376afcdb97e9481925f6ea2e
Baks1mio <lightmb0@gmail.com>
2024-10-31 22:06:24 +0300

    final

--- dist/js/_form.js
+++
@@ -1,7 +0,0 @@
-const inputs = document.querySelectorAll('.form__text-input');
-
-inputs.forEach((item) => {
-    item.addEventListener('blur', (e) => {
-        console.log('Working');
-    });
-});
--- dist/js/scipt.js
+++
@@ -1,6 +0,0 @@
-document.DOMContentLoaded(() => {
-
-
-
-
-});
